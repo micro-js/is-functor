b5e14a5b2a80d6b72475bf34f78dedfd7b4ae2ca
Josh Taylor <joshrtay@gmail.com>
2016-01-27 13:59:16 -0800

    add deps and tests

--- test/index.js
+++ test/index.js
@@ -9,6 +9,12 @@ var test = require('tape')
  * Tests
  */
 
-test('should work', function (t) {
+test('should return true for array', function (t) {
+  t.equal(isFunctor([]), true)
+  t.end()
+})
 
+test('should return false for obj', function (t) {
+  t.equal(isFunctor({}), false)
+  t.end()
 })
